da707f6f1e6aa7d8875907949adf8c955521754e
Julio Ojeda <jcom.94m@gmail.com>
2020-08-09 21:44:20 -0700

    one more try

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 
 const express = require('express');
+const path = require('path');
 
 var app = express();
 var port = process.env.PORT || 5000;
 
 app.use(express.static('build'))
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build/index.html'), function (err) {
+    if (err) {
+      res.status(500).send(err)
+    }
+  })
+})
 
 app.listen(port, () => {
   console.log(`Started  on port ${port}`);
